58cc29c088cd42aa42ba7b12ecbb55600b8219b8
moravas <moravas8051@gmail.com>
2014-12-07 14:44:21 +0100

    queue register class
    It targets to manage registering communication queues between threads.

---
+++ communication/ipcQueue.hpp
@@ -0,0 +1,28 @@
+//! =============================================================================
+//! \copyright
+//! \section
+//! \subsection
+//! \brief
+//! \author         Norbert Toth
+//! \date			07.12.2014.
+//! \details
+//! \note
+//! =============================================================================
+#ifndef _IPCQUEUE_HPP_
+#define _IPCQUEUE_HPP_
+
+namespace ipc {
+
+class ipcQueue {
+
+public: ipcQueue();
+
+};
+
+} // ipc
+
+#endif //_IPCQUEUE_HPP_
+
+//! =============================================================================
+//! \copyright
+//! ======================= end of file: ipcQueue.cpp ===========================
