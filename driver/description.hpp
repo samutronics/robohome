a262e6f2b6440e145b7396c5f563353a3d55218e
moravas <moravas8051@gmail.com>
2014-12-08 18:11:51 +0100

    Description file added to the "driver" directory, which tell the developer, that it is third party library.

---
+++ driver/description.hpp
@@ -0,0 +1,18 @@
+//! =============================================================================
+//! \copyright
+//! \section		API documentation
+//! \subsection
+//! \brief
+//! \author         Norbert Toth
+//! \date			07.12.2014.
+//! \details		This directory holds the hardware dependent definitions
+//!						and sources for peripherial drivers, that were
+//!						copied from TivaWare_C_Series driver library.
+//! =============================================================================
+#ifndef _DESCRIPTION_HPP_
+#define _DESCRIPTION_HPP_
+
+#endif // _DESCRIPTION_HPP_
+//! =============================================================================
+//! \copyright
+//! ======================= end of file: ipcQueue.cpp ===========================
