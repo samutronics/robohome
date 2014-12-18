75f7a329d0ee562f95f95cff76bf5003ec17768f
moravas <moravas8051@gmail.com>
2014-12-18 17:19:26 +0100

    RTC working model

--- managers/rtcmanager.cpp
+++ managers/rtcmanager.cpp
@@ -14,28 +14,34 @@
 using namespace communication::ipc;
 using namespace manager::rtcTask;
 
+
+//! \note wokraround, there was an compiler failure, if I included the time.h
+//!			and I tried to use its tm struct.
+
+struct tm
+{
+    int tm_sec;      /* seconds after the minute   - [0,59]  */
+    int tm_min;      /* minutes after the hour     - [0,59]  */
+    int tm_hour;     /* hours after the midnight   - [0,23]  */
+    int tm_mday;     /* day of the month           - [1,31]  */
+    int tm_mon;      /* months since January       - [0,11]  */
+    int tm_year;     /* years since 1900                     */
+    int tm_wday;     /* days since Sunday          - [0,6]   */
+    int tm_yday;     /* days since Jan 1st         - [0,365] */
+    int tm_isdst;    /* Daylight Savings Time flag           */
+};
+
 rtcManager::rtcManager() {
-	SysCtlPeripheralEnable(SYSCTL_PERIPH_HIBERNATE);
 	HibernateEnableExpClk(system::currentSystemClockFrequency);
-	SysCtlDelay(1000000);
-	HibernateClockConfig(HIBERNATE_OSC_LOWDRIVE);
-	HibernateRTCEnable();
-	//
-	// Set the RTC to an initial value.
-	//
-	HibernateRTCSet(0);
-	//
-	// Set Match 0 for 30 seconds from now.
-	//
-	HibernateRTCMatchSet(0, 30);
-	//
-	// Set up interrupts on the Hibernation module to enable the RTC match
-	// 0 interrupt. Clear all pending interrupts and register the
-	// interrupt handler.
-	//
-	HibernateIntEnable(HIBERNATE_INT_RTC_MATCH_0);
+	HibernateCounterMode(HIBERNATE_COUNTER_24HR);
+
+	tm t;
+	t.tm_min = 0xFF;
+	HibernateCalendarMatchSet(0, &t);
 	HibernateIntClear(HIBERNATE_INT_PIN_WAKE | HIBERNATE_INT_LOW_BAT | HIBERNATE_INT_RTC_MATCH_0);
+	HibernateIntEnable(HIBERNATE_INT_RTC_MATCH_0);
 	HibernateIntRegister(&rtcManager::handlerTH);
+	HibernateRTCEnable();
 }
 
 void rtcManager::task(void *pvParameters) {
@@ -46,7 +52,6 @@ void rtcManager::task(void *pvParameters) {
 }
 
 void rtcManager::handlerTH() {
-	while(true);
 	TimerIntClear(TIMER0_BASE, TIMER_RTC_MATCH);
 }
 
