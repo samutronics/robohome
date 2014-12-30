f712d52156b0b92cce363b4cdd65a6734249e836
moravas <moravas8051@gmail.com>
2014-12-30 19:24:11 +0100

    default manager implementation

--- managers/exosite/exositemanager.hpp
+++ managers/exosite/exositemanager.hpp
@@ -2,55 +2,29 @@
 //! \file
 //! \brief
 //! \author         Norbert Toth
-//! \date			29.12.2014.
+//! \date			30.12.2014.
 //! \note
 // =============================================================================
-#ifndef _WEATHERMANAGER_H_
-#define _WEATHERMANAGER_H_
+#ifndef _EXOSITEMANAGER_H_
+#define _EXOSITEMANAGER_H_
 
 #include "imanager.hpp"
-#include "weatherreport.hpp"
-#include "weatherrequestfactory.hpp"
-#include "../projectconfiguration.hpp"
 
 namespace manager {
-namespace weatherTask {
+namespace exositeTask {
 
-class weatherManager: public IManager {
-private: weatherManager();
+class exositeManager: public IManager {
+private: exositeManager();
 private: virtual void task(void *pvParameters);
-TO_BE_RUNABLE(weatherManager)
-
-private: static err_t connectToServer();
-private: static err_t sendRequest();
-
-// =============================================================================
-// Callback methods
-// =============================================================================
-
-private: static void resolveHostCallback(const char *pcName, struct ip_addr *psIPAddr, void *vpArg);
-private: static err_t connectToServerCallback(void *pvArg, struct tcp_pcb *psPcb, err_t iErr);
-
-private: static err_t TCPReceiveCallback(void *pvArg, struct tcp_pcb *psPcb, struct pbuf *psBuf, err_t iErr);
-private: static err_t TCPSentCallback(void *pvArg, struct tcp_pcb *psPcb, u16_t ui16Len);
-private: static void TCPErrorCallback(void *vPArg, err_t iErr);
-
-// =============================================================================
-// Member declarations
-// =============================================================================
-
-private: static report			_report;
-private: static ip_addr			_serverIP;
-private: static tcp_pcb*		_pcb;
-private: static weatherRequest	_request;
+TO_BE_RUNABLE(exositeManager)
 };
 
-} // weatherTask
+} // exositeTask
 
 } // manager
 
-#endif // _WEATHERMANAGER_H_
+#endif // _EXOSITEMANAGER_H_
 // =============================================================================
 //! \file
 //! \copyright
-// ==================== end of file: weathermanager.hpp ========================
+// ==================== end of file: exositemanager.hpp ========================
