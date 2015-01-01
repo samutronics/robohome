fd18d37f5727d49b234f4d767864075d22ce12f9
moravas <moravas8051@gmail.com>
2015-01-01 20:08:30 +0100

    Exosite header and footer

--- managers/exosite/exosite.cpp
+++ managers/exosite/exosite.cpp
@@ -1,40 +1,12 @@
-/*****************************************************************************
-*
-* exosite.c - Exosite cloud communications.
-* Copyright (c) 2013, Exosite LLC
-* All rights reserved.
-* 
-* Redistribution and use in source and binary forms, with or without 
-* modification, are permitted provided that the following conditions are met:
-* 
-*     * Redistributions of source code must retain the above copyright notice,
-*       this list of conditions and the following disclaimer.
-*     * Redistributions in binary form must reproduce the above copyright 
-*       notice, this list of conditions and the following disclaimer in the
-*       documentation and/or other materials provided with the distribution.
-*     * Neither the name of Exosite LLC nor the names of its contributors may
-*       be used to endorse or promote products derived from this software 
-*       without specific prior written permission.
-* 
-* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-* AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
-* IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-* ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE 
-* LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
-* CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
-* SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-* INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
-* CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
-* ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-* POSSIBILITY OF SUCH DAMAGE.
-*
-*****************************************************************************/
-
+// =============================================================================
+//! \file
+//! \brief
+//! \author         Norbert Toth
+//! \date			01.01.2015.
+//! \note
+// =============================================================================
 #include "exosite_hal_lwip.h"
 #include "exosite_meta.h"
-
-
-//#include "utils/ustdlib.h"
 
 #include "exosite.hpp"
 
@@ -586,3 +558,8 @@ void exosite::sendLine(long socket, unsigned char LINE, const char* payload) {
 
 	  return;
 }
+
+// =============================================================================
+//! \file
+//! \copyright
+// ========================= end of file: exosite.cpp ==========================
