477dca3deba8bb891392bab3ce5ca409d7c3bbc3
moravas <moravas8051@gmail.com>
2015-01-04 11:26:52 +0100

    header and footer are added to the basicvector

--- containers/basicvector.hpp
+++ containers/basicvector.hpp
@@ -1,16 +1,20 @@
-/*
- * basicvector.hpp
- *
- *  Created on: 2015.01.02.
- *      Author: moravas
- */
-
-#ifndef BASICVECTOR_HPP_
-#define BASICVECTOR_HPP_
+// =============================================================================
+//! \file
+//! \brief
+//! \author         Norbert Toth
+//! \date			02.01.2015.
+//! \note
+// =============================================================================
+#ifndef _BASICVECTOR_HPP_
+#define _BASICVECTOR_HPP_
 
 template<typename t, unsigned int size> class basicVector {
 public: t container[size];
 public: unsigned int len;
 };
 
-#endif /* BASICVECTOR_HPP_ */
+#endif // _BASICVECTOR_HPP_
+// =============================================================================
+//! \file
+//! \copyright
+// ======================= end of file: basicvector.cpp ========================
