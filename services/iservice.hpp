1290cec29037b75bbe089db09162e1ce7eefa6b2
moravas <moravas8051@gmail.com>
2015-01-04 15:21:25 +0100

    IService interface documentation

--- services/iservice.hpp
+++ services/iservice.hpp
@@ -12,12 +12,25 @@
 
 namespace service {
 
+// =============================================================================
+//! \brief Interface for tasks
+//!
+//!	The interface provide the method, that is harmed to the requirement of FreeRTOS
+//! threads
+// =============================================================================
 class IService {
 	protected: virtual void task(void *pvParameters) = 0;
 };
 
-}  // namespace manager
+}  // service
 
+// =============================================================================
+//! \brief C source capable thread implementation for wrapping the interface implementations
+//!
+//!	Due to C sources cannot work on C++ obejct, C++ statis method needs to
+//! wrap the thread implementation. Furthermore, only one thread is required and
+//! allowed per type. To fulfill that, a special singleton design pattern is applied.
+// =============================================================================
 #define TO_BE_RUNABLE(derived)									\
 	public: static inline void start(void *pvParameters) {		\
 		static derived d;										\
@@ -25,10 +38,20 @@ class IService {
 		while(1);												\
 	};															\
 
+// =============================================================================
+//! \brief Definition of Top Halve part of THBH model
+//!
+//!	This macro provides implementation for the Top Halve interrupt handler. There
+//! is a statical method definition for interrupt handling, and a queue for
+//! communication the two halve.
+// =============================================================================
 #define DEFINE_TH												\
 		private: static void handlerTH();						\
 		private: static xQueueHandle _THQueue;
 
+// =============================================================================
+//! \brief Declaration of the queue in the Top Halve part of THBH model
+// =============================================================================
 #define DECLARE_TH(derived)										\
 		xQueueHandle derived::_THQueue;
 
