655185506125fcb80faf6d862eaef77db1b1e4e7
moravas <moravas8051@gmail.com>
2015-01-13 20:26:34 +0100

    Remove unnecessary static methods from exosite. It seems like an OOP class...

--- services/exosite/exosite.cpp
+++ services/exosite/exosite.cpp
@@ -15,11 +15,6 @@
 using namespace service::exosite;
 using namespace service::exosite::configuration;
 
-ip_addr									exosite::_serverIP;
-tcp_pcb*								exosite::_pcb;
-exosite::state							exosite::_state = idle;
-basicVector<u8, exosite::_rxTxBufSize>	exosite::_rxTxBuf;
-
 exosite::exosite() {
 }
 
@@ -48,27 +43,22 @@ void exosite::task(void *pvParameters) {
 	while(1) {
 		deviceRequestFactory::makeDeviceSyncRequest();
 
-		if((0 != deviceRequestFactory::writeRequestOutbound.len) && (_state == idle)) {
+		if(0 != deviceRequestFactory::writeRequestOutbound.len) {
 			exositeRequestFactory::write(deviceRequestFactory::writeRequestOutbound, _rxTxBuf);
 			if (ERR_OK != netconn_write(connection, _rxTxBuf.container, _rxTxBuf.len, NETCONN_COPY)) {UARTprintf("failed to write into connection\n"); while(1);}
-			_state = writeRequestSent;
 			_rxTxBuf.len = 0;
 
 			netbuf* buf = NULL;
 			if (ERR_OK != netconn_recv(connection, &buf)) {UARTprintf("failed to receive from connection\n"); while(1);}
 			exositeRequestFactory::parseWriteResult(buf->p);
 			netbuf_delete(buf);
-
-//			while(writeRequestProcessed != _state) {taskYIELD();}
-			_state = idle;
 		}
 
 		vTaskDelay(updatePeriode);
 
-		if((0 != deviceRequestFactory::readRequestOutbound.len) && (_state == idle)) {
+		if(0 != deviceRequestFactory::readRequestOutbound.len) {
 			exositeRequestFactory::read(deviceRequestFactory::readRequestOutbound, _rxTxBuf);
 			if (ERR_OK != netconn_write(connection, _rxTxBuf.container, _rxTxBuf.len, NETCONN_COPY)) {UARTprintf("failed to write into connection\n"); while(1);}
-			_state = readRequestSent;
 			_rxTxBuf.len = 0;
 			netbuf* buf = NULL;
 			if (ERR_OK != netconn_recv(connection, &buf)) {UARTprintf("failed to receive from connection\n"); while(1);}
@@ -98,129 +88,10 @@ void exosite::task(void *pvParameters) {
 
 		    	taskEXIT_CRITICAL();
 		    }
-
-		    _state = idle;
 		}
 
 		vTaskDelay(updatePeriode);
 	}
-}
-
-void exosite::closeConnection(tcp_pcb* psPcb) {
-	if(psPcb) {
-		//
-		// Clear out all of the TCP callbacks.
-		//
-		tcp_sent(psPcb, 0);
-		tcp_recv(psPcb, 0);
-		tcp_err(psPcb, 0);
-
-		//
-		// Close the TCP connection.
-		//
-		tcp_close(psPcb);
-        if(psPcb == _pcb) {
-            _pcb = 0;
-        }
-	}
-}
-
-err_t exosite::connectToServer() {
-	closeConnection(_pcb);
-
-	//
-	// Create a new TCP socket.
-	//
-	_pcb = tcp_new();
-
-    //
-    // Attempt to connect to the server directly.
-    //
-    return tcp_connect(_pcb, &_serverIP, port, connectToServerCallback);
-}
-
-err_t exosite::sendRequest() {
-    err_t retVal = tcp_write(_pcb, _rxTxBuf.container, _rxTxBuf.len, TCP_WRITE_FLAG_COPY);
-
-	//
-    //  Write data for sending (but does not send it immediately).
-    //
-    if(retVal == ERR_OK) {
-        //
-        // Find out what we can send and send it
-        //
-        retVal = tcp_output(_pcb);
-    }
-
-    return retVal;
-}
-
-err_t exosite::connectToServerCallback(void *pvArg, struct tcp_pcb *psPcb, err_t iErr) {
-    //
-    // Check if there was a TCP error.
-    //
-    if(iErr != ERR_OK) {
-    	closeConnection(psPcb);
-        return (ERR_OK);
-    }
-
-    tcp_recv(psPcb, TCPReceiveCallback);
-    tcp_err(psPcb, TCPErrorCallback);
-    tcp_sent(psPcb, TCPSentCallback);
-
-    return(ERR_OK);
-}
-
-err_t exosite::TCPReceiveCallback(void* pvArg, struct tcp_pcb* psPcb, struct pbuf* psBuf, err_t iErr) {
-	struct pbuf *psBufCur;
-	if(!psBuf) {
-		closeConnection(psPcb);
-		return(ERR_OK);
-	}
-
-	if(writeRequestSent == _state) {
-		_state = writeRequestProcessed;
-		exositeRequestFactory::parseWriteResult(psBuf);
-	}
-	else if(readRequestSent == _state) {
-		_state = readRequestProcessed;
-		exositeRequestFactory::parseReadResult(psBuf, deviceRequestFactory::response);
-	}
-
-	//
-	// Initialize the linked list pointer to parse.
-	//
-	psBufCur = psBuf;
-
-	//
-	// Free the buffers used since they have been processed.
-	//
-	while(psBufCur->len != 0) {
-		//
-		// Indicate that you have received and processed this set of TCP data.
-		//
-		tcp_recved(psPcb, psBufCur->len);
-
-		//
-		// Go to the next buffer.
-		//
-		psBufCur = psBufCur->next;
-
-		//
-		// Terminate if there are no more buffers.
-		//
-		if(!psBufCur) {break;}
-	}
-
-	//
-	// Free the memory space allocated for this receive.
-	//
-	pbuf_free(psBuf);
-	return(ERR_OK);
-}
-
-err_t exosite::TCPSentCallback(void* pvArg, struct tcp_pcb* psPcb, u16_t ui16Len) {return (ERR_OK);}
-void exosite::TCPErrorCallback(void*, err_t) {
 }
 
 // =============================================================================
--- services/exosite/exosite.hpp
+++ services/exosite/exosite.hpp
@@ -20,38 +20,11 @@ private: exosite();
 private: virtual void task(void *pvParameters);
 TO_BE_RUNABLE(exosite)
 
-private: static err_t connectToServer();
-private: static err_t sendRequest();
-private: static void closeConnection(tcp_pcb* psPcb);
-
-// =============================================================================
-// Callback methods
-// =============================================================================
-
-private: static err_t connectToServerCallback(void *pvArg, struct tcp_pcb *psPcb, err_t iErr);
-
-private: static err_t TCPReceiveCallback(void *pvArg, struct tcp_pcb *psPcb, struct pbuf *psBuf, err_t iErr);
-private: static err_t TCPSentCallback(void *pvArg, struct tcp_pcb *psPcb, u16_t ui16Len);
-private: static void TCPErrorCallback(void *vPArg, err_t iErr);
-
 // =============================================================================
 // Member declarations
 // =============================================================================
-
-private: static const u16						_rxTxBufSize = 4096;
-private: static basicVector<u8, _rxTxBufSize>	_rxTxBuf;
-private: static ip_addr							_serverIP;
-private: static tcp_pcb*						_pcb;
-
-private: enum state {
-	readRequestSent,
-	readRequestProcessed,
-	writeRequestSent,
-	writeRequestProcessed,
-	idle
-};
-
-private: static state		_state;
+private: basicVector<u8, 4096>	_rxTxBuf;
+private: ip_addr				_serverIP;
 };
 
 } // exosite
