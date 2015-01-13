e1428fa78f61c56f805327c71afc6bae0567597a
moravas <moravas8051@gmail.com>
2015-01-13 18:51:38 +0100

    weather request became a real OOP class, and it seems to work well with the netconn API.

--- services/weather/weather.cpp
+++ services/weather/weather.cpp
@@ -13,12 +13,6 @@
 using namespace service::weather;
 using namespace service::weather::configuration;
 
-report			weather::_report;
-ip_addr			weather::_serverIP;
-tcp_pcb*		weather::_pcb;
-weatherRequest	weather::_request;
-bool			weather::_tcpRequestReceived;
-
 weather::weather() {
 }
 
@@ -58,6 +52,9 @@ void weather::task(void *pvParameters) {
 		if (ERR_OK != netconn_recv(connection, &buf)) {UARTprintf("failed to receive from connection\n"); while(1);}
 
 		u32 itemCount = JSONParseCurrent(0, _report, buf->p);
+
+		netbuf_delete(buf);
+
 		if(0 < itemCount) {
 			//Guard the message printing.
 			taskENTER_CRITICAL();
@@ -69,172 +66,16 @@ void weather::task(void *pvParameters) {
 		else {
 			UARTprintf("Failed to parse request");
 		}
+
 		netconn_close(connection);
 		netconn_delete(connection);
+
 		// =============================================================================
 		//! * Wait until the time of the next request
 		// =============================================================================
 		vTaskDelay(updatePeriode);
 	}
 }
-
-void weather::closeConnection(tcp_pcb* psPcb) {
-	if(psPcb) {
-		//
-		// Clear out all of the TCP callbacks.
-		//
-		tcp_sent(psPcb, 0);
-		tcp_recv(psPcb, 0);
-		tcp_err(psPcb, 0);
-
-		//
-		// Close the TCP connection.
-		//
-		tcp_close(psPcb);
-        if(psPcb == _pcb) {
-            _pcb = 0;
-        }
-	}
-}
-
-err_t weather::connectToServer() {
-	closeConnection(_pcb);
-
-	//
-    // Create a new TCP socket.
-    //
-    _pcb = tcp_new();
-
-    //
-    // Attempt to connect to the server directly.
-    //
-    return tcp_connect(_pcb, &_serverIP, port, connectToServerCallback);
-}
-
-err_t weather::sendRequest() {
-    err_t retVal = tcp_write(_pcb, _request.request, _request.len, TCP_WRITE_FLAG_COPY);
-
-    //
-    //  Write data for sending (but does not send it immediately).
-    //
-    if(retVal == ERR_OK) {
-        //
-        // Find out what we can send and send it
-        //
-        retVal = tcp_output(_pcb);
-    }
-
-    return retVal;
-}
-
-err_t weather::connectToServerCallback(void *pvArg, struct tcp_pcb *psPcb, err_t iErr) {
-    //
-    // Check if there was a TCP error.
-    //
-    if(iErr != ERR_OK) {
-    	closeConnection(psPcb);
-        return (ERR_OK);
-    }
-
-    tcp_recv(psPcb, TCPReceiveCallback);
-    tcp_err(psPcb, TCPErrorCallback);
-    tcp_sent(psPcb, TCPSentCallback);
-
-    return(ERR_OK);
-}
-
-// =============================================================================
-//! If an empty buffer arrived, the callbeck will close the socket, and notify
-//! the service about the successful processing. In any other cases, checks the
-//! request type stored locally and parse as JSON object.
-//! In case of successful parsing, the important report items will be wrtitten
-//! into the terminal.
-// =============================================================================
-err_t weather::TCPReceiveCallback(void* pvArg, struct tcp_pcb* psPcb, struct pbuf* psBuf, err_t iErr) {
-	struct pbuf *psBufCur;
-	int32_t i32Items;
-
-	if(!psBuf) {
-		closeConnection(psPcb);
-		return(ERR_OK);
-	}
-
-	if(_request.type == currentRequest) {
-		//
-		// Read items from the buffer.
-		//
-		i32Items = JSONParseCurrent(0, _report, psBuf);
-
-		//
-		// Make sure some items were found.
-		//
-		if(i32Items > 0) {
-			//Guard the message printing.
-			taskENTER_CRITICAL();
-			UARTprintf("Temperature: %d C\n",	_report.Temp);
-			UARTprintf("Humidity: %d %%\n",		_report.Humidity);
-			UARTprintf("Pressure: %d hpa\n",	_report.Pressure);
-			taskEXIT_CRITICAL();
-		}
-		else if(i32Items < 0) {
-			UARTprintf("Invalid request arrived\n");
-		}
-	}
-	else if(_request.type == forecastRequest) {
-		//
-		// Read items from the buffer.
-		//
-		i32Items = JSONParseForecast(0, _report, psBuf);
-
-		if(i32Items > 0) {
-			//Guard the message printing.
-			taskENTER_CRITICAL();
-			UARTprintf("Temperature: %d C\n",	_report.Temp);
-			UARTprintf("Humidity: %d %%\n",		_report.Humidity);
-			UARTprintf("Pressure: %d hpa\n",	_report.Pressure);
-			taskEXIT_CRITICAL();
-		}
-		else if(i32Items < 0) {
-			UARTprintf("Invalid request arrived\n");
-		}
-	}
-
-	_tcpRequestReceived = true;
-
-	//
-	// Initialize the linked list pointer to parse.
-	//
-	psBufCur = psBuf;
-
-	//
-	// Free the buffers used since they have been processed.
-	//
-	while(psBufCur->len != 0) {
-		//
-		// Indicate that you have received and processed this set of TCP data.
-		//
-		tcp_recved(psPcb, psBufCur->len);
-
-		//
-		// Go to the next buffer.
-		//
-		psBufCur = psBufCur->next;
-
-		//
-		// Terminate if there are no more buffers.
-		//
-		if(psBufCur == 0) {break;}
-	}
-
-	//
-	// Free the memory space allocated for this receive.
-	//
-	pbuf_free(psBuf);
-	return(ERR_OK);
-}
-
-err_t weather::TCPSentCallback(void* pvArg, struct tcp_pcb* psPcb, u16_t ui16Len) {return (ERR_OK);}
-void weather::TCPErrorCallback(void*, err_t) {}
 
 // =============================================================================
 //! \file
--- services/weather/weather.hpp
+++ services/weather/weather.hpp
@@ -31,55 +31,11 @@ private: virtual void task(void *pvParameters);
 TO_BE_RUNABLE(weather)
 
 // =============================================================================
-//! \brief Try to establish connection to the server.
-//!
-//! First of all, clear existing connection, make a new socket, and send a tcp
-//! connection request.
-// =============================================================================
-private: static err_t connectToServer();
-
-// =============================================================================
-//! \brief Call tcp write and flush together.
-// =============================================================================
-private: static err_t sendRequest();
-
-// =============================================================================
-//! \brief Completely clear the socket, and try to erase the local socket object
-// =============================================================================
-private: static void closeConnection(tcp_pcb* psPcb);
-
-// =============================================================================
-//! \brief Finalize the connection request
-//!
-//! Check the error state, and in case of any error, close the socket. If the
-//! connection was established successful, callbacks will be registered in the
-//! socket object.
-// =============================================================================
-private: static err_t connectToServerCallback(void *pvArg, struct tcp_pcb *psPcb, err_t iErr);
-
-// =============================================================================
-//! \brief Callback method for data receiving via TCP socket
-// =============================================================================
-private: static err_t TCPReceiveCallback(void *pvArg, struct tcp_pcb *psPcb, struct pbuf *psBuf, err_t iErr);
-
-// =============================================================================
-//! \brief Dummy implementation for notification: TCP packet has been sent
-// =============================================================================
-private: static err_t TCPSentCallback(void *pvArg, struct tcp_pcb *psPcb, u16_t ui16Len);
-
-// =============================================================================
-//! \brief Dummy implementation for notification: Error occured
-// =============================================================================
-private: static void TCPErrorCallback(void *vPArg, err_t iErr);
-
-// =============================================================================
 // Member declarations
 // =============================================================================
-private: static report			_report;
-private: static ip_addr			_serverIP;
-private: static tcp_pcb*		_pcb;
-private: static weatherRequest	_request;
-private: static bool			_tcpRequestReceived;
+private: report			_report;
+private: ip_addr		_serverIP;
+private: weatherRequest	_request;
 };
 
 } // weather
