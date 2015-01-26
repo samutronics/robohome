5cf8f1773d7c9021202648eb5a4f9acd3015b23f
moravas <moravas8051@gmail.com>
2015-01-26 19:24:50 +0100

    default implementation of abstract service transmitter.

---
+++ communication/asbtractservicerequesttransmitter.cpp
@@ -0,0 +1,15 @@
+// =============================================================================
+//! \file
+//! \brief
+//! \author         Norbert Toth
+//! \date			26.01.2015.
+//! \note
+// =============================================================================
+#include "asbtractservicerequesttransmitter.hpp"
+
+using namespace communication;
+
+// =============================================================================
+//! \file
+//! \copyright
+// ============ end of file: asbtractservicerequesttransmitter.cpp =============
---
+++ communication/asbtractservicerequesttransmitter.hpp
@@ -0,0 +1,22 @@
+// =============================================================================
+//! \file
+//! \brief
+//! \author         Norbert Toth
+//! \date			26.01.2015.
+//! \note
+// =============================================================================
+#ifndef _ASBTRACTSERVICEREQUESTTRANSMITTER_H_
+#define _ASBTRACTSERVICEREQUESTTRANSMITTER_H_
+
+namespace communication {
+
+class asbtractServiceRequestTransmitter {
+};
+
+} // communication
+
+#endif // _ASBTRACTSERVICEREQUESTTRANSMITTER_H_
+// =============================================================================
+//! \file
+//! \copyright
+// ============ end of file: asbtractservicerequesttransmitter.hpp =============
