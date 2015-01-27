2ecce6635564a86804e4539306efe987dd207cc5
moravas <moravas8051@gmail.com>
2015-01-27 19:40:58 +0100

    default implementation. It seems to be a solid code base for the future development.

--- communication/asbtractservicerequesttransmitter.cpp
+++ communication/asbtractservicerequesttransmitter.cpp
@@ -8,7 +8,63 @@
 #include "asbtractservicerequesttransmitter.hpp"
 
 using namespace communication;
+using namespace systemGlobal;
 
+void asbtractServiceRequestTransmitter::retryContext(netconn*& connection, s32& error) {
+	while(IPGatheringStrategie == IPADDR_USE_DHCP && 0x0 == lwIPLocalIPAddrGet() || 0xFFFFFFFF == lwIPLocalIPAddrGet()) {taskYIELD();}
+
+	// =============================================================================
+	//! * At this point, the excecution has to wait for the end of the
+	//!		inicialization of the dns module. Until that, argument error will be
+	//!		returned from the dns lookup.
+	//!	\note Refactoring of each of LwIP services would be nice to became queriable
+	//! about its state, or the service send any kind of event.
+	// =============================================================================
+	error = netconn_gethostbyname(_url, &_serverIP);
+	if(ERR_OK != error) {return;}
+
+	while(1) {
+		// =============================================================================
+		//! * Create HTTP get request to query the actual weather informations. It has
+		//! to be done only once.
+		//! \warning Whitespaces are not allowed in the the request.
+		// =============================================================================
+		netbuf* request = generateRequest();
+
+		// =============================================================================
+		//! .
+		//! The further steps are excecuted periodically.
+		//! * Connect to the server, and block the execution until the connection will
+		//! be established.
+		// =============================================================================
+		connection = netconn_new(_connectionType);
+		if (connection == NULL) {return;}
+
+		error = netconn_connect(connection, &_serverIP, _port);
+		if (ERR_OK != error) {return;}
+
+		_connectionType == NETCONN_TCP ?
+				error = netconn_write(connection, request->p->payload, request->p->tot_len, NETCONN_NOCOPY) :
+				error = netconn_send(connection, request);
+		if (ERR_OK != error) {return;}
+
+		netbuf* buf = NULL;
+		error = netconn_recv(connection, &buf);
+		if (ERR_OK != error) {netbuf_delete(buf); return;}
+
+		if(!processingReply(buf)) {
+			return;
+		}
+
+		netconn_close(connection);
+		netconn_delete(connection);
+
+		// =============================================================================
+		//! * Wait until the time of the next request
+		// =============================================================================
+		vTaskDelay(_updatePeriode);
+	}
+}
 // =============================================================================
 //! \file
 //! \copyright
--- communication/asbtractservicerequesttransmitter.hpp
+++ communication/asbtractservicerequesttransmitter.hpp
@@ -7,11 +7,33 @@
 // =============================================================================
 #ifndef _ASBTRACTSERVICEREQUESTTRANSMITTER_H_
 #define _ASBTRACTSERVICEREQUESTTRANSMITTER_H_
+#include "../projectconfiguration.hpp"
+
+class netbuf;
+class netconn;
 
 namespace communication {
 
 class asbtractServiceRequestTransmitter {
+protected: inline asbtractServiceRequestTransmitter(const char* url, u16 port, netconn_type type, u32 updatePeriode);
+
+protected: void retryContext(netconn*& connection, s32& error);
+
+protected: virtual bool processingReply(netbuf* reply) = 0;
+protected: virtual netbuf* generateRequest() = 0;
+
+private: const char*		_url;
+private: const u16			_port;
+private: ip_addr			_serverIP;
+private: const u32			_updatePeriode;
+private: const netconn_type	_connectionType;
 };
+
+inline asbtractServiceRequestTransmitter::asbtractServiceRequestTransmitter(const char* url, u16 port, netconn_type type, u32 updatePeriode):
+		_url(url),
+		_port(port),
+		_updatePeriode(updatePeriode),
+		_connectionType(type) {}
 
 } // communication
 
