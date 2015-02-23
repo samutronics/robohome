ef245ddc9be40d93c7bef6251167eda6e4aaf24b
moravas <moravas8051@gmail.com>
2015-02-23 20:01:19 +0100

    separated requirement page has been added.

---
+++ doc/requirements.hpp
@@ -0,0 +1,18 @@
+// =============================================================================
+//! \file
+//! \brief			Documentation page
+//! \author         Norbert Toth
+//! \date			21.02.2015.
+//! \note
+// =============================================================================
+//!
+//! \page requirements Requirements
+//! \tableofcontents
+//! \section requirement Requirements
+//!
+//! * <b>Get weather informations:</b> The device should query weather informations from the http://openweathermap.org
+//!
+// =============================================================================
+//! \file
+//! \copyright
+// ===================== end of file: requirements.hpp =========================
