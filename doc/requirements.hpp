c80ac56f64feb34e9b64d383f6b9b140b2fa807d
moravas <moravas8051@gmail.com>
2015-02-27 15:43:27 +0100

    architecture documentations are added. Draft version, refine them during development.

--- doc/requirements.hpp
+++ doc/requirements.hpp
@@ -8,9 +8,55 @@
 //!
 //! \page requirements Requirements
 //! \tableofcontents
+//! \section openissue Open issues
+//!
+//! -# The client wants to manipulate same output from multiple input. What is the prefered evaluation
+//! order if multiple changes occured at same time?
+//! -# The client preferes the "last win" evaluation strategie in case of tri-state outputs. It could be dangerous,
+//! becasue the IO read / write is done via polling. Data loss can be occured.
+//! -# The client wants to make a logical binding betwen the irrigation and IO management. Suggestion: it should be
+//! implemented in different component.
+//!
+//! \section userstory User stories
+//! -# Project related user stories:
+//!     -# I, as a commissioner, would like to have a project structure that holds various configuration options.
+//!     -# I, as a commissioner, would like to configure the IP address gathering and the IP address itself in the project.
+//!     -# I, as a commissioner, would like to define the number of inputs and their properties in the project.
+//!     -# I, as a commissioner, would like to define the number of outputs and their properties in the project.
+//!     -# I, as a commissioner, would like to define connections between the inputs and outputs.
+//! -# Hardware input related user stories:
+//!     -# I, as a commissioner would like an input to reacts for both of 0->1 and 1->0 changes.
+//!     -# I, as a commissioner would like an input to reacts only for the 0->1 input change.
+//! -# Hardware output related user stories:
+//!     -# I, as a commissioner would like to configure the output to changes the state of one specific output bit immeditly.
+//!     The initial state should be the inactive.
+//!     -# I, as a commissioner would like to configure the output to turn on the state of one specific output bit immeditly
+//!     and turn off with time-out. The initial state should be the inactive and the time-out is adjustable during commissioning.
+//!     -# I, as a commissioner would like to configure the output to turn on the state of one specific output bit with time-out
+//!     and turn off inmediatly. The initial state should be the inactive and the time-out is adjustable during commissioning.
+//!     -# I, as a commissioner would like to define groups of outputs that are controlled by one specific input.
+//!     -# I, as a commissioner would like to configure an output that's controlled by many inputs.
+//!     -# I, as a commissioner would like to have an output that controls two bits with the states:
+//!         * both turned off
+//!         * first turned on, second turned off
+//!         * first turned off, second turned on
+//!         .
+//!     and between the passive -> active transition a delay should be applied.
+//!
 //! \section requirement Requirements
 //!
 //! * <b>Get weather informations:</b> The device should query weather informations from the http://openweathermap.org
+//! * <b>Supported input types:</b>
+//!     * simple input: stateless, reacts on changes
+//!     * push button: react only on 0->1 change
+//! * <b>Supported output types:</b>
+//!     * simple timed:
+//!         * deferred state change possibility
+//!         * configurable time-out
+//!         * maximal time-out: 2^16*0.1 ~ 4.55 days
+//!     * tri state timed:
+//!         * 2 outputs with exclusive OR
+//!         * state change delayed 500ms
 //!
 // =============================================================================
 //! \file
--- doc/systemarchitecture.hpp
+++ doc/systemarchitecture.hpp
@@ -5,14 +5,141 @@
 //! \date			22.12.2014.
 //! \note
 // =============================================================================
-
-//! \page sds SDS
+//!
+//! \page systemarchitecture System architecture
 //! \tableofcontents
-//! \section requirement Requirements
 //!
 //!	\section designconstraints Design constraints
-//! There are several design rules and contrains for developing embedded systems, so we can discuss only about the
-//! most importants.
+//! -# There is no separeted input for the services. Services act as an write request from the input side.
+//! -# The number of inputs and outputs are must be identical as the IO points in the hardware configuration
+//!
+//!	\section projectmanagement Project management
+//! The project is the human readable, text represented form that determines the run-time behavioral of the system related to its inputs and outputs.
+//!
+//!	\section ioarchitecture IO architecture
+//!	The most important part of the system is the IO management. It's composed of two parts, the run-time model and the configuration management.
+//! The run-time management covers the class and object model of run-time classes, properties and interactions with each other. The
+//! configuration management determines how the compiled projects are interpreted and transformed into run-time objects.
+//!
+//! \subsection runtime Run-time model
+//! There are two major requiremnts to the IO handling:
+//! * The ablity to the IO reconfiguring both their properties and/or their numbers.
+//! * The high performance in relation of the evaluation time.
+//!
+//! Observing the real use cases, the user required properties are represented at the ouptput side. Furthermore, the outputs are stateless objects,
+//! that means that they don't hold theirs output states. Because of timed ouptputs can be represented in the system, decrease of theirs timers must
+//! be guaranted. In order to be done, the following options have to be considered:
+//! -# The processing goes through all of the inputs and each input objects notify the subscribed outputs.
+//!		* pros:
+//! 		* easy to implement
+//! 		* easy to deploy a new project
+//! 	* cons:
+//! 		* the processing has to go trough all of the inputs even if there wasn't input change.
+//! -# Every kind of timed outputs are stored in separeted arrays encapsulated of the input that's processed in separete step.
+//! 	* pros:
+//! 		* the non-timed outputs won't be evaluated
+//! 	* cons:
+//! 		* deployment sequence is more complicated
+//! 		* a bit higher memory consumption because the memory allocation has been done in more steps
+//! .
+//! Considering the performance requirements, the second solution is prefered. It resoults the following evaluation sequence:
+//!
+//!
+//! \subsubsection input Input
+//! The main interaction of the inputs is the write operation on that from the direction of services. The main consumer
+//! service is the IOTask that reads datas from the input boards and assigns it to the inputs. The communications is done
+//! via SPI that's byte oriented.<br>
+//! Considering the facts above, the SimpleInput class encapsualtes two array to store the current and the previous value
+//! of the given input. The arrays are related to class instead of the object to be guaranted the uniqness whitin the
+//! system. Two kind of write operation has to be possible relating to the inputs:
+//! * Write a single bit is needed by the services, like webservice.
+//! * Write a mass data is required for the IO task.
+//! The inputs hold a trigger property that determines the kind of input which will be interpreted as input change. If an
+//! appropriate change occured considering the trigger property, the connected outputs will be notified.
+//!
+//! \startuml
+//! enum triggerMode {
+//! triggerBothEdges
+//! triggerRisingEdges
+//! triggerFallingEdges
+//! }
+//!
+//! class SimpleInput {
+//! - triggerMode _trigger;
+//! - std::vector<SimpleOutput*> _connected;
+//! - {static} std::vector<unsigned char> _inputCurrent;
+//! - {static} std::vector<unsigned char> _inputPrevious;
+//! + SimpleInput(triggerMode t);
+//! + void write();
+//! + {static} void write(std::vector<unsigned char>& massData);
+//! + void notifyOutputs();
+//! }
+//!
+//! \enduml
+//!
+//! \subsubsection output Output
+//! The system has two kind of output that can fulfill every requirements. Like SimpleInput above, the real data is in
+//! a static array, and its address stored in the SimpleOutput object. Optionally, a time agument passed to the notification
+//! can deffere the change of output cames into effect on the hardware.<br>
+//! Construct time an argument adjust the timing behavioral of the output. If this parameter is zero, the timer property
+//! is turned off. The following timing behavioral has to be implemented by the SimpleOutput class:
+//!
+//! Deferred timer  |   Timeout | Object state  |   Output
+//! --------------- | --------- | ------------- | ---------
+//!         On      |   On      |   passive     |   x
+//!         On      |   Off     |   passive     |   x
+//!         Off     |   On      |   passive     |   x
+//!         Off     |   Off     |   passive     |   x
+//!         On      |   On      |   active      |   x
+//!         On      |   Off     |   active      |   x
+//!         Off     |   On      |   active      |   x
+//!         Off     |   Off     |   active      |   x
+//!         On      |   On      |   wait        |   x
+//!         On      |   Off     |   wait        |   x
+//!         Off     |   On      |   wait        |   x
+//!         Off     |   Off     |   wait        |   x
+//!         On      |   On      |   activeUP    |   x
+//!         On      |   Off     |   activeUP    |   x
+//!         Off     |   On      |   activeUP    |   x
+//!         Off     |   Off     |   activeUP    |   x
+//!         On      |   On      |   activeDown  |   x
+//!         On      |   Off     |   activeDown  |   x
+//!         Off     |   On      |   activeDown  |   x
+//!         Off     |   Off     |   activeDown  |   x
+//!
+//!
+//! \startuml
+//!
+//! enum outputState {
+//! passive
+//! active
+//! wait
+//! activeUP
+//! activeDown
+//! }
+//!
+//! class SimpleOutput {
+//! # outputState _state;
+//! # unsigned short _hwAddress;
+//! # unsigned short _timeOut;
+//! # unsigned short _timer;
+//! # unsigned short _deferredTimer;
+//! # {static} std::vector<unsigned char> _output;
+//! + SimpleOutput(unsigned short timeOut);
+//! + notify(unsigned short deferredTime);
+//! }
+//!
+//! class TriStateOutput {
+//! - unsigned short _extendedHwAddress;
+//! - unsigned char _stateChangeDelay;
+//! }
+//!
+//! SimpleOutput <|-- TriStateOutput
+//! \enduml
+//!
+//!
+//! \subsection configuration Configuration management
+//!
 //!
 //!
 //!
@@ -96,4 +223,4 @@ Useful examples:
 // =============================================================================
 //! \file
 //! \copyright
-// ========================= end of file: sds.hpp ==============================
+// ================= end of file: systemarchitecture.hpp =======================
