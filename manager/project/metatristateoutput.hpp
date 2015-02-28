b2b67e1cb63e7b43e77c3d1386eb12b9ad468f6b
moravas <moravas8051@gmail.com>
2015-02-28 18:36:48 +0100

    metatristateoutput draft implementation.

---
+++ manager/project/metatristateoutput.hpp
@@ -0,0 +1,173 @@
+// =============================================================================
+//! \file
+//! \brief
+//! \author         Norbert Toth
+//! \date			28.02.2015.
+//! \note
+// =============================================================================
+#ifndef _METATRISTATEOUTPUT_HPP_
+#define _METATRISTATEOUTPUT_HPP_
+
+#include "metaoutput.hpp"
+#include "../projectconfiguration.hpp"
+
+namespace manager {
+namespace project {
+
+class mataTriStateOutput: public metaOutput {
+public: inline mataTriStateOutput(const u16 sectionAddress, const u16 itemAddress);
+
+public: inline virtual void next();
+
+public: inline u16 extendedAddress() const;
+public: inline void inputsDown(std::vector<u16>& down) const;
+public: inline void inputsUp(std::vector<u16>& up) const;
+
+private: inline u16 inputDownCount() const;
+private: inline u16 inputUpCount() const;
+};
+
+inline mataTriStateOutput::mataTriStateOutput(const u16 sectionAddress): metaOutput(sectionAddress) {}
+
+inline void mataTriStateOutput::next() {
+	// the total item size in the EEPROM composed of:
+	_itemAddress +=
+			// the size of the address property
+			sizeof(u16) +
+			// the size of the time-out property
+			sizeof(u16) +
+			// the size of the UpDown input count property
+			sizeof(u16) +
+			// the number of connected UpDown input count property
+			inputCount() * sizeof(u16) +
+			// the size of the extendedAddress property
+			sizeof(u16) +
+			// the size of the Down input count property
+			sizeof(u16) +
+			// the number of connected Down input count property
+			inputDownCount() * sizeof(u16) +
+			// the size of the Up input count property
+			sizeof(u16) +
+			// the number of connected Up input count property
+			inputUpCount() * sizeof(u16);
+}
+
+inline u16 mataTriStateOutput::extendedAddress() const {
+	u32 value;
+	// The start address of extendedAddress in the EEPROM is composed of:
+	const u32 address =
+			// the _itemAddress
+			_itemAddress +
+			// the size of the address property
+			sizeof(u16) +
+			// the size of the time-out property
+			sizeof(u16) +
+			// the size of the input count property
+			sizeof(u16) +
+			// the number of connected UpDown input count property
+			inputCount() * sizeof(u16);
+
+	EEPROMRead(&value, address / sizeof(u32), 1);
+	return static_cast<u16>(((value >> address % sizeof(u32) * 8) & 0xFFFF));
+}
+
+inline void mataTriStateOutput::inputsDown(std::vector<u16>& down) const {
+	// The start address of inputsDown in the EEPROM is composed of:
+	const u32 address =
+			// the _itemAddress
+			_itemAddress +
+			// the size of the address property
+			sizeof(u16) +
+			// the size of the time-out property
+			sizeof(u16) +
+			// the size of the input count property
+			sizeof(u16) +
+			// the number of connected UpDown input count property
+			inputCount() * sizeof(u16) +
+			// the size of the extendedAddress property
+			sizeof(u16) +
+			// the size of the Down input count property
+			sizeof(u16);
+
+	inputReader(down, address, inputDownCount());
+}
+
+inline void mataTriStateOutput::inputsUp(std::vector<u16>& up) const {
+	// The start address of inputsUp in the EEPROM is composed of:
+	const u32 address =
+			// the _itemAddress
+			_itemAddress +
+			// the size of the address property
+			sizeof(u16) +
+			// the size of the time-out property
+			sizeof(u16) +
+			// the size of the UpDown input count property
+			sizeof(u16) +
+			// the number of connected UpDown input count property
+			inputCount() * sizeof(u16) +
+			// the size of the extendedAddress property
+			sizeof(u16) +
+			// the size of the Down input count property
+			sizeof(u16) +
+			// the number of connected Down input count property
+			inputDownCount() * sizeof(u16) +
+			// the size of the Up input count property
+			sizeof(u16);
+
+	inputReader(down, address, inputUpCount());
+}
+
+inline u16 mataTriStateOutput::inputDownCount() const {
+	u32 value;
+	// The start address of inputDownCount in the EEPROM is composed of:
+	const u32 address =
+			// the _itemAddress
+			_itemAddress +
+			// the size of the address property
+			sizeof(u16) +
+			// the size of the time-out property
+			sizeof(u16) +
+			// the size of the input count property
+			sizeof(u16) +
+			// the number of connected UpDown input count property
+			inputCount() * sizeof(u16) +
+			// the size of the extendedAddress property
+			sizeof(u16);
+
+	EEPROMRead(&value, address / sizeof(u32), 1);
+	return static_cast<u16>(((value >> address % sizeof(u32) * 8) & 0xFFFF));
+}
+
+inline u16 mataTriStateOutput::inputUpCount() const {
+	u32 value;
+	// The start address of inputUpCount in the EEPROM is composed of:
+	const u32 address =
+			// the _itemAddress
+			_itemAddress +
+			// the size of the address property
+			sizeof(u16) +
+			// the size of the time-out property
+			sizeof(u16) +
+			// the size of the UpDown input count property
+			sizeof(u16) +
+			// the number of connected UpDown input count property
+			inputCount() * sizeof(u16) +
+			// the size of the extendedAddress property
+			sizeof(u16) +
+			// the size of the Down input count property
+			sizeof(u16) +
+			// the number of connected Down input count property
+			inputDownCount() * sizeof(u16);
+
+	EEPROMRead(&value, address / sizeof(u32), 1);
+	return static_cast<u16>(((value >> address % sizeof(u32) * 8) & 0xFFFF));
+}
+
+} // project
+} // manager
+
+#endif // _METATRISTATEOUTPUT_HPP_
+// =============================================================================
+//! \file
+//! \copyright
+// ===================== end of file: metatristateoutput.hpp ===================
