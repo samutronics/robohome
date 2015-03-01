1c352507674bce16c711bcf2a4311318830b6e34
moravas <moravas8051@gmail.com>
2015-03-01 08:32:30 +0100

    projectmanager type is added

---
+++ manager/project/projectmanager.cpp
@@ -0,0 +1,24 @@
+// =============================================================================
+//! \file
+//! \brief
+//! \author         Norbert Toth
+//! \date			28.02.2015.
+//! \note
+// =============================================================================
+#include "projectmanager.hpp"
+
+using namespace manager::project;
+
+ProjectManager* ProjectManager::_instance;
+
+void ProjectManager::read(std::vector<u32>& project) const {
+
+}
+
+bool ProjectManager::write(const std::vector<u32>& project) const {
+	return false;
+}
+// =============================================================================
+//! \file
+//! \copyright
+// ======================= end of file: projectmanager.cpp =====================
---
+++ manager/project/projectmanager.hpp
@@ -0,0 +1,100 @@
+// =============================================================================
+//! \file
+//! \brief
+//! \author         Norbert Toth
+//! \date			28.02.2015.
+//! \note
+// =============================================================================
+#ifndef _PROJECTMANAGER_HPP_
+#define _PROJECTMANAGER_HPP_
+
+#include "metainput.hpp"
+#include "metaoutput.hpp"
+#include "metasysconfig.hpp"
+#include "metairrigation.hpp"
+#include "metatristateoutput.hpp"
+#include "../projectconfiguration.hpp"
+
+namespace manager {
+namespace project {
+
+class ProjectManager {
+private: static inline ProjectManager* getInstance();
+
+public: void read(std::vector<u32>& project) const;
+public: bool write(const std::vector<u32>& project) const;
+
+public: inline metaInput			input() const;
+public: inline metaOutput			output() const;
+public: inline metaTriStateOutput	triStateOutput() const;
+public: inline metaSysConfig		sysConfig() const;
+public: inline metaIrrigation		irrigation() const;
+
+private: inline ProjectManager();
+
+private: static ProjectManager* _instance;
+};
+
+inline ProjectManager* ProjectManager::getInstance() {
+	if(!_instance) {
+		xSemaphoreHandle sync = NULL;
+		if(!sync) {
+			sync = xSemaphoreCreateMutex();
+		}
+
+		 xSemaphoreTake(sync, 0);
+		if(!_instance) {
+			_instance = new ProjectManager();
+		}
+
+		xSemaphoreGive(sync);
+	}
+
+	return _instance;
+}
+
+inline ProjectManager::ProjectManager() {
+	SysCtlPeripheralEnable(SYSCTL_PERIPH_EEPROM0);
+	EEPROMInit();
+}
+
+inline metaSysConfig ProjectManager::sysConfig() const {
+	u32 section;
+	EEPROMRead(&section, 0, 1);
+	return metaSysConfig(0);
+}
+
+inline metaInput ProjectManager::input() const {
+	u32 section;
+	EEPROMRead(&section, 0, 1);
+	return metaInput(static_cast<u16>((section >> 16) & 0xFFFF));
+}
+
+inline metaOutput ProjectManager::output() const {
+	u32 section;
+	EEPROMRead(&section, 1, 1);
+	EEPROMRead(&section, ((section & 0xFFFF) / sizeof(u32)), 1);
+	return metaOutput(static_cast<u16>(section >> (((section & 0xFFFF) % sizeof(u32)) * 8) & 0xFFFF));
+}
+
+inline metaTriStateOutput ProjectManager::triStateOutput() const {
+	u32 section;
+	EEPROMRead(&section, 1, 1);
+	EEPROMRead(&section, (((section >> 16) & 0xFFFF) / sizeof(u32)), 1);
+	return metaTriStateOutput(static_cast<u16>(section >> (((section & 0xFFFF) % sizeof(u32)) * 8) & 0xFFFF));
+}
+
+inline metaIrrigation ProjectManager::irrigation() const {
+	u32 section;
+	EEPROMRead(&section, 1, 1);
+	return metaIrrigation(0);
+}
+
+} // project
+} // manager
+
+#endif // _PROJECTMANAGER_HPP_
+// =============================================================================
+//! \file
+//! \copyright
+// ======================= end of file: projectmanager.hpp =====================
