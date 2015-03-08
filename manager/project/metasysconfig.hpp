630d9f467c72acadee4a63281d2e61601fa390b4
moravas <moravas8051@gmail.com>
2015-03-08 15:39:43 +0100

    metasysconfig prepared to manage the CIK for TI IoT.

--- manager/project/metasysconfig.hpp
+++ manager/project/metasysconfig.hpp
@@ -15,12 +15,32 @@ namespace project {
 
 class metaSysConfig {
 public: metaSysConfig(cu16 sectionAddress): _sectionAddress(sectionAddress) {}
+public: inline void cik(std::string& c) const;
 private: cu16 _sectionAddress;
 };
 
 // =============================================================================
 // Inline method implementation
 // =============================================================================
+
+inline void metaSysConfig::cik(std::string& c) const {
+	u32 tmp[2];
+	EEPROMRead(tmp, _sectionAddress - (_sectionAddress % sizeof(tmp[0])), sizeof(tmp));
+
+	u32 cikSize;
+	if(2 < _sectionAddress % sizeof(tmp[0])) {
+		cikSize = ((tmp[1] & 0XFF) << 8) | (tmp[0] >> ((_sectionAddress % sizeof(tmp[0])) * 8) & 0XFF);
+	}
+	else {
+		cikSize = (tmp[0] >> ((_sectionAddress % sizeof(tmp[0])) * 8) & 0XFFFF);
+	}
+
+	for(u32 index = 0; index < cikSize; index++) {
+		u32 character;
+		EEPROMRead(&character, (_sectionAddress + sizeof(u16) + index) - ((_sectionAddress + sizeof(u16) + index) % sizeof(character)), sizeof(character));
+		c += ((character >> (((_sectionAddress + sizeof(u16) + index) % sizeof(character)) * 8)) & 0XFF);
+	}
+}
 
 } // project
 } // manager
