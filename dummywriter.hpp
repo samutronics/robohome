77f2b93a269c23baa5dc5508d3f489a190b22528
moravas <moravas8051@gmail.com>
2015-03-10 19:10:42 +0100

    project generation seems to work correctly.

--- dummywriter.hpp
+++ dummywriter.hpp
@@ -8,7 +8,6 @@
 #ifndef DUMMYWRITER_HPP_
 #define DUMMYWRITER_HPP_
 
-#include "metainput.hpp"
 #include "../projectconfiguration.hpp"
 
 //make system configuration
@@ -16,16 +15,16 @@ static cs8 cik[] = "15e4890a88d79261608b194c8217c486c68f0007";
 static cu16 cikSize = (sizeof(cik) / sizeof(cik[0]));
 
 inline void makeSysCfg(std::vector<u8>& project) {
-	project.push_back(static_cast<u8>((cikSize >> 8) | 0XFF));
-	project.push_back(static_cast<u8>(cikSize | 0XFF));
-	for(u32 index = 2; index < sizeof(cik); index++) {
+	project.push_back(static_cast<u8>(cikSize & 0XFF));
+	project.push_back(static_cast<u8>((cikSize >> 8) & 0XFF));
+	for(u32 index = 0; index < sizeof(cik); index++) {
 		project.push_back(cik[index]);
 	}
 }
 
 //make input properties
 static cu8 input[] = {
-		manager::project::triggerBothEdges,
+		manager::project::triggerRisingEdge,
 		manager::project::triggerBothEdges,
 		manager::project::triggerBothEdges,
 		manager::project::triggerBothEdges,
@@ -43,12 +42,11 @@ static cu8 input[] = {
 		manager::project::triggerRisingEdge
 };
 
-static cu16 inputCount = sizeof(input) / sizeof(input[0]);
-
 inline void makeInputCfg(std::vector<u8>& project) {
-	project.push_back(static_cast<u8>((inputCount >> 8) | 0XFF));
-	project.push_back(static_cast<u8>(inputCount | 0XFF));
-	for(u32 index = 2; index < sizeof(input); index++) {
+	cu16 inputCount = sizeof(input) / sizeof(input[0]);
+	project.push_back(static_cast<u8>(inputCount & 0XFF));
+	project.push_back(static_cast<u8>((inputCount >> 8) & 0XFF));
+	for(u32 index = 0; index < sizeof(input); index++) {
 		project.push_back(input[index]);
 	}
 }
@@ -58,37 +56,37 @@ static cu16 simpleO[] = {
 		// address, time-out, input-count, input list
 		0,	0,		1,		0,
 		1,	200,	2,		1, 2,
-		2,	0,		3,		4, 6,
+		2,	0,		2,		4, 6,
 		3,	0,		1,		7
 };
 
-static cu16 simpleOutputCount = sizeof(simpleO) / sizeof(simpleO[0]);
+static cu16 simpleOutputCount = 4;
 
 inline void makeSimpleOutputCfg(std::vector<u8>& project) {
-	project.push_back(static_cast<u8>((simpleOutputCount >> 8) | 0XFF));
-	project.push_back(static_cast<u8>(simpleOutputCount | 0XFF));
-	for(u32 index = 2; index < simpleOutputCount; index++) {
-		project.push_back(static_cast<u8>((simpleO[index] >> 8) | 0XFF));
-		project.push_back(static_cast<u8>(simpleO[index] | 0XFF));
+	project.push_back(static_cast<u8>(simpleOutputCount & 0XFF));
+	project.push_back(static_cast<u8>((simpleOutputCount >> 8) & 0XFF));
+	for(u32 index = 0; index < sizeof(simpleO) / sizeof(simpleO[0]); index++) {
+		project.push_back(static_cast<u8>(simpleO[index] & 0XFF));
+		project.push_back(static_cast<u8>((simpleO[index] >> 8) & 0XFF));
 	}
 }
 
 //make tri-state output properties
 static cu16 triStateO[] = {
 		// address, time-out, UpDown-count, UpDown list, extendedaddress, Down-count, Down list, Up-count, Up list,
-		4,	500,	1,		5,		5,		0,		0,		0,		0,
+		4,	500,	1,		5,		5,		0,				0,
 		6,	0,		0,				7,		1,		8,		1,		9,
 		8,	0,		0,				9,		2,		10, 11,	2,		12, 13
 };
 
-static cu16 triStateOutputCount = sizeof(triStateO) / sizeof(triStateO[0]);
+static cu16 triStateOutputCount = 3;
 
 inline void makeTriStateOutputCfg(std::vector<u8>& project) {
-	project.push_back(static_cast<u8>((triStateOutputCount >> 8) | 0XFF));
-	project.push_back(static_cast<u8>(triStateOutputCount | 0XFF));
-	for(u32 index = 2; index < triStateOutputCount; index++) {
-		project.push_back(static_cast<u8>((triStateO[index] >> 8) | 0XFF));
-		project.push_back(static_cast<u8>(triStateO[index] | 0XFF));
+	project.push_back(static_cast<u8>(triStateOutputCount & 0XFF));
+	project.push_back(static_cast<u8>((triStateOutputCount >> 8) & 0XFF));
+	for(u32 index = 0; index < sizeof(triStateO) / sizeof(triStateO[0]); index++) {
+		project.push_back(static_cast<u8>(triStateO[index] & 0XFF));
+		project.push_back(static_cast<u8>((triStateO[index] >> 8) & 0XFF));
 	}
 }
 
@@ -99,6 +97,18 @@ inline void makeCfg(std::vector<u32>& project) {
 	cu16 inputAddress		= sysCfgAddress + data.size();
 	makeInputCfg(data);
 	cu16 outputAddress		= sysCfgAddress + data.size();
+
+	// push the total size of sections
+	data.push_back(static_cast<u8>((sizeof(simpleO) + sizeof(simpleOutputCount)) & 0XFF));
+	data.push_back(static_cast<u8>(((sizeof(simpleO) + sizeof(simpleOutputCount))>> 8) & 0XFF));
+	data.push_back(static_cast<u8>((sizeof(triStateO) + sizeof(triStateOutputCount)) & 0XFF));
+	data.push_back(static_cast<u8>(((sizeof(triStateO) + sizeof(triStateOutputCount)) >> 8) & 0XFF));
+
+
+	UARTprintf("output section address: %d", outputAddress);
+	UARTprintf("simple output section size: %d", (sizeof(simpleO) + sizeof(simpleOutputCount)));
+	UARTprintf("tri-state output section size: %d", (sizeof(triStateO) + sizeof(triStateOutputCount)));
+
 	makeSimpleOutputCfg(data);
 	makeTriStateOutputCfg(data);
 	cu16 irrigationAddress	= sysCfgAddress + data.size();
@@ -110,9 +120,9 @@ inline void makeCfg(std::vector<u32>& project) {
 	}
 
 	if(data.size() % sizeof(project[0])) {
-		u32 last;
+		u32 last = 0;
 		for(u32 index = data.size() - (data.size() % sizeof(project[0])); index < data.size(); index++) {
-			last |= data[index] << index % sizeof(project[0]);
+			last |= data[index] << ((index % sizeof(project[0])) * 8);
 		}
 
 		project.push_back(last);
