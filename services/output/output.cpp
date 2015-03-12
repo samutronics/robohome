47794155773c01511a0bc9564b20ccbd7225eb08
moravas <moravas8051@gmail.com>
2015-03-12 18:17:17 +0100

    output service ready for test

--- services/output/output.cpp
+++ services/output/output.cpp
@@ -8,14 +8,43 @@
 #include "output.hpp"
 #include "../projectconfiguration.hpp"
 
+using namespace manager::output;
+using namespace manager::project;
 using namespace service::outbound;
 using namespace service::outbound::configuration;
 
 DECLARE_TH(output)
 
-output::output() {
+output::output():
+		_dataByteCount(ProjectManager::getInstance()->sysConfig().hwOutputNumber() / 8),
+		_outputManager(OutputManager::getInstance()) {
 	_THQueue = xSemaphoreCreateBinary();
 
+	timerStart();
+	IOStart();
+}
+
+void output::task(void *pvParameters) {
+	while(1) {
+		// The thread gives up its time-slice, if there is no semaphire given.
+		xSemaphoreTake(_THQueue, portMAX_DELAY);
+
+		const std::vector<Output*>& out = _outputManager->outputs();
+		for(u32 index = 0; index < out.size(); index++) {
+			out[index]->evaluate();
+		}
+
+		IOWrite();
+
+//		UARTprintf("D\n");
+
+
+		// The task gives up its remained time-slice
+		taskYIELD();
+	}
+}
+
+void output::timerStart() const {
 	SysCtlPeripheralEnable(timerPeriphery);
 	TimerConfigure	(timer, TIMER_CFG_PERIODIC);
 	TimerLoadSet	(timer, TIMER_A, systemGlobal::requestedSystemClockFrequency / pollingFrequency);
@@ -24,18 +53,28 @@ output::output() {
 	TimerEnable		(timer, TIMER_A);
 }
 
-void output::task(void *pvParameters) {
-	while(1) {
-		// The thread gives up its time-slice, if there is no semaphire given.
-		xSemaphoreTake(_THQueue, portMAX_DELAY);
+void output::IOStart() const {
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_SSI1);
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOB);
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOE);
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOH);
 
+    GPIOPinConfigure(GPIO_PB4_SSI1FSS); 		//CS
+    GPIOPinConfigure(GPIO_PB5_SSI1CLK); 		//CLK
+    GPIOPinConfigure(GPIO_PE4_SSI1XDAT0);		//MOSI
+    GPIOPinConfigure(GPIO_PE5_SSI1XDAT1);		//MISO
 
-		UARTprintf("D\n");
+    GPIOPinTypeSSI(GPIO_PORTB_BASE, GPIO_PIN_4 | GPIO_PIN_5);
+    GPIOPinTypeSSI(GPIO_PORTE_BASE, GPIO_PIN_4 | GPIO_PIN_5);
 
+    GPIOPinTypeGPIOOutput(GPIO_PORTH_BASE, GPIO_PIN_2);
+    LoadOFF();
+    GPIOPadConfigSet(GPIO_PORTH_BASE, GPIO_PIN_2 ,GPIO_STRENGTH_12MA, GPIO_PIN_TYPE_STD);
+    GPIOPinTypeGPIOInput(GPIO_PORTH_BASE, GPIO_PIN_1);
 
-		// The task gives up its remained time-slice
-		taskYIELD();
-	}
+	SSIConfigSetExpClk(SSI1_BASE, systemGlobal::requestedSystemClockFrequency, SSI_FRF_MOTO_MODE_0, SSI_MODE_MASTER, 500, 8);
+	SSIEnable(SSI1_BASE);
+	LoadON();
 }
 
 void output::handlerTH() {
--- services/output/output.hpp
+++ services/output/output.hpp
@@ -9,20 +9,50 @@
 #define _OUTPUT_H_
 
 #include "iservice.hpp"
+#include "outputmanager.hpp"
+#include "projectmanager.hpp"
 #include "../projectconfiguration.hpp"
 
 namespace service {
 namespace outbound {
 
-class output: public IService{
+class output: public IService {
 private: output();
 private: virtual void task(void *pvParameters);
+
+private: void timerStart() const;
+private: void IOStart() const;
+private: inline void IOWrite() const;
+private: inline void LoadON() const;
+private: inline void LoadOFF() const;
+
+private: cu32 _dataByteCount;
+private: manager::output::OutputManager* _outputManager;
+
 TO_BE_RUNABLE(output)
 DEFINE_TH
 };
 
-} // outbound
+inline void output::LoadON() const {
+	GPIOPinWrite(GPIO_PORTH_BASE, GPIO_PIN_2, GPIO_PIN_2);
+}
 
+inline void output::LoadOFF() const {
+	GPIOPinWrite(GPIO_PORTH_BASE, GPIO_PIN_2, 0x0);
+}
+
+inline void output::IOWrite() const {
+	const std::vector<u32>& out = _outputManager->read();
+	LoadOFF();
+	for(u32 index = 0; index < _dataByteCount; index++) {
+		SSIDataPut(SSI1_BASE, (out[(_dataByteCount / sizeof(out[0])) - (index / sizeof(out[0]))] >> ((_dataByteCount - 1 - (index % _dataByteCount)) * 8)) & 0XFF);
+		while(SSIBusy(SSI1_BASE)) {taskYIELD();}
+	}
+
+	LoadON();
+}
+
+} // outbound
 } // service
 
 #endif // _OUTPUT_H_
